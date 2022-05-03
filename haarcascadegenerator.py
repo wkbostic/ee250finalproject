64955f30109c4ed6ab2669d3b487dc24a8f6d90f
Kristian Vu Bostic <wbostic@usc.edu>
2022-05-03 15:45:18 -0700

    cascade finished

--- haarcascadegenerator.py
+++ haarcascadegenerator.py
@@ -1,1 +1,15 @@
+from icrawler.builtin import BingImageCrawler
 
+# Find positive images
+#classes=['red solo cup'] 
+#number=200
+#for c in classes:
+    #bing_crawler=BingImageCrawler(storage={'root_dir':f'p/{c.replace(" ",".")}'})
+    #bing_crawler.crawl(keyword=c,filters=None,max_num=number,offset=0)
+
+# Find negative images
+classes=['trees','roads','Human faces', 'red ball', 'red shirt']
+number=500
+for c in classes:
+    bing_crawler2=BingImageCrawler(storage={'root_dir':f'n/{c.replace(" ",".")}'})
+    bing_crawler2.crawl(keyword=c,filters=None,max_num=number,offset=0)
